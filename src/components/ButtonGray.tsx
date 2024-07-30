8f10e3e0b7e4507725c07f993291e2dc4764ff5a
Sharad <sharadshekar@gmail.com>
2024-07-30 10:52:54 -0300

    styling

--- src/components/ButtonGray.tsx
+++ src/components/ButtonGray.tsx
@@ -4,25 +4,23 @@ interface Props {
   className?: string;
   border?: string;
   onClick?: () => void;
+  variant?: 'default' | 'text';
 }
 
 const ButtonGray: React.FC<React.PropsWithChildren<Props>> = ({
   onClick,
   className,
+  variant = 'default',
   ...props
 }) => {
+  const buttonClass =
+    variant === 'text'
+      ? 'flex w-full items-center space-x-2 bg-transparent  py-6 px-6 rounded-8 text-gray-dark-11 hover:bg-gray-dark-5 disabled:bg-transparent '
+      : `${props.border} inline-block w-full rounded-12 bg-gray-dark-4 px-32 py-16 hover:bg-gray-dark-5 sm:w-fit`;
+
   return (
-    <button
-      className={`${props.border} inline-block w-full rounded-12 bg-gray-dark-4 px-32 py-16 hover:bg-gray-dark-5 sm:w-fit`}
-      onClick={onClick}
-    >
-      <Text
-        as="span"
-        style="btn-l"
-        className={`text-gray-dark-11 ${className}`}
-      >
-        {props.children}
-      </Text>
+    <button className={buttonClass} onClick={onClick}>
+      {props.children}
     </button>
   );
 };
