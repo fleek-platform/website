fe35bafb46da9c25e6bb5769435e26e98047a67b
Victor F. Santos <victor.gaard@gmail.com>
2024-09-11 18:57:58 -0300

    feat: new navbar

--- src/components/v2/Navbar/Navbar.tsx
+++ src/components/v2/Navbar/Navbar.tsx
@@ -4,9 +4,9 @@ import Link from '@components/Link';
 
 export const Navbar: React.FC = () => {
   return (
-    <nav className="flex justify-between">
-      <p>Fleek</p>
-      <div>
+    <nav className="flex justify-between rounded-12 border border-gray-dark-3 px-12 py-8 text-14 font-medium text-gray-dark-11">
+      <img src="/svg/fleek-logo.svg" width={66} alt="fleek logo" />
+      <div className="flex items-center gap-36">
         {navbarMenu.map((navbarItem) => (
           <Link>{navbarItem.label}</Link>
         ))}
