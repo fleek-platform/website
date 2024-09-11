381043dbcf2d7776d97b05b3e7df31fde2825dc3
Victor F. Santos <victor.gaard@gmail.com>
2024-09-11 19:50:26 -0300

    feat: hero section mock

---
+++ src/components/v2/Announcement/Announcement.tsx
@@ -0,0 +1,7 @@
+export const Announcement: React.FC = () => {
+  return (
+    <div className="mr-auto rounded-8 bg-gray-dark-2 px-8 py-6 font-plex-sans text-16 font-normal text-gray-dark-12">
+      ✨ New: our public IPFS gateway
+    </div>
+  );
+};
--- src/components/v2/Navbar/Navbar.tsx
+++ src/components/v2/Navbar/Navbar.tsx
@@ -4,16 +4,26 @@ import Link from '@components/Link';
 
 export const Navbar: React.FC = () => {
   return (
-    <nav className="flex justify-between rounded-12 border border-gray-dark-3 px-12 py-8 text-14 font-medium text-gray-dark-11">
+    <nav className="flex justify-between rounded-12 border border-gray-dark-3 bg-gray-dark-1 px-12 py-8 font-plex-sans text-14 font-medium text-gray-dark-11">
       <img src="/svg/fleek-logo.svg" width={66} alt="fleek logo" />
       <div className="flex items-center gap-36">
         {navbarMenu.map((navbarItem) => (
-          <Link>{navbarItem.label}</Link>
+          <Link
+            key={navbarItem.label}
+            href={navbarItem.url}
+            className="cursor-pointer hover:text-gray-dark-12"
+          >
+            {navbarItem.label}
+          </Link>
         ))}
       </div>
-      <div>
-        <button>Log in</button>
-        <button>Sign up</button>
+      <div className="flex items-center gap-8">
+        <button className="h-32 rounded-8 bg-gray-dark-3 px-8 text-gray-dark-11">
+          Log in
+        </button>
+        <button className="h-32 rounded-8 bg-white px-8 text-gray-dark-1">
+          Sign up
+        </button>
       </div>
     </nav>
   );
