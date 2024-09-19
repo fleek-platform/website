2bab988831038c4f4b9a16005b4b35821a9478e7
Victor F. Santos <victor.gaard@gmail.com>
2024-09-19 17:57:43 -0300

    style: ready to live section

--- src/components/ReadyToLive.tsx
+++ src/components/ReadyToLive.tsx
@@ -1,40 +1,22 @@
 import React from 'react';
-import Container from './Container';
-import PageSection from './PageSection';
-import ButtonYellow from './ButtonYellow';
-import type { RoundedType } from '@components/PageSection';
-import Link from './Link';
-import Text from './Text';
+import { Container } from './v2/LandingPage/Container/Container';
+import { Button } from './v2/Button/Button';
+import { Text } from './v2/LandingPage/Text/Text';
 
-interface Props {
-  rounded?: RoundedType;
-}
-
-const ReadyToLive: React.FC<Props> = ({ rounded }) => {
+const ReadyToLive: React.FC = () => {
   return (
-    <Container>
-      <PageSection rounded={rounded}>
-        <div className={'relative pb-64 pt-24 lg:px-42 lg:py-54 '}>
-          <h2 className="typo-h5 mx-64 text-center text-gray-dark-12 lg:typo-h4 lg:mx-0">
-            Are you on Fleek yet?
-          </h2>
-          <div className="mx-64 mt-32 flex justify-center lg:mx-0">
-            <Link href="https://app.fleek.xyz" className="w-full lg:w-fit">
-              <ButtonYellow
-                border="border-yellow"
-                className="flex items-center justify-center gap-12 "
-              >
-                <div>
-                  <img src="/svg/bolt-yellow.svg" />
-                </div>
-                <Text as="span" className="btn-l">
-                  Get started
-                </Text>
-              </ButtonYellow>
-            </Link>
-          </div>
-        </div>
-      </PageSection>
+    <Container
+      gradient="right"
+      className="flex flex-col items-center space-y-40"
+    >
+      <Text as="h3">Are you on Fleek yet?</Text>
+      <Button
+        href="https://app.fleek.xyz"
+        variant="app-primary"
+        className="w-fit"
+      >
+        Get started with Fleek
+      </Button>
     </Container>
   );
 };
