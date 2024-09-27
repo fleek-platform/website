796f55ef0c04389105c32c77b19a5beeaf4307a2
Victor F. Santos <victor.gaard@gmail.com>
2024-09-27 12:47:17 -0300

    chore: remove overage from pricing table

--- src/components/PricingCard.tsx
+++ src/components/PricingCard.tsx
@@ -37,7 +37,6 @@ const PricingCard: React.FC<Props> = (props) => {
           <p className="typo-m text-left text-gray-dark-12">
             $ <span className="typo-h5">{props.cost}</span> /mo
           </p>
-          <p className="typo-s mt-3 text-left">+ Overage</p>
         </div>
         <div className="my-12 bg-gray-dark-6 p-[1px]" />
         <div className="mb-16">
