fe1a9c08f0f545d0976c11c5aee16d09c0475d06
tobySolutions <adedejitobiloba7@gmail.com>
2024-10-15 19:24:42 +0100

    chore: pulling

--- src/utils/generateCanonicalUrl.ts
+++ src/utils/generateCanonicalUrl.ts
@@ -1,9 +1,5 @@
 export function generateCanonicalUrl(baseUrl: string, slug?: string) {
   if (!slug) return baseUrl;
 
-  if (slug === 'docs/') {
-    return `${baseUrl}/${slug}`;
-  }
-
-  return `${baseUrl}/${slug}/`;
+  return `${baseUrl}/${slug.replace(/\/$/, '')}/`;
 }
