13481a40cfdea0c634980c9c703a7192643335c1
azula000 <105008968+azula000@users.noreply.github.com>
2024-10-23 11:21:08 -0300

    Update links

--- src/components/Support/config.ts
+++ src/components/Support/config.ts
@@ -23,34 +23,34 @@ export const singleArticleCardItems = [
   {
     id: '1',
     title: 'Node engine errors',
-    link: '/references/faq/node-engine-errors',
+    link: '/references/faq/node-engine-errors/',
     description:
       "If you are getting node engine mismatch errors, don't worry. It only means the docker image you selected does not have the required node version. However, you can use any docker image published on dockerhub, you don't need to use the ones we provide if...",
   },
   {
     id: '2',
-    link: '/guides/hosting-on-ipfs-best-practices-troubleshooting',
+    link: '/guides/hosting-on-ipfs-best-practices-troubleshooting/',
     title: 'Best Practices for Building on Fleek.',
     description:
       'Dive into best practices for building IPFS-hosted websites, with specific attention to offline-first strategy, utilizing the Service Worker API, the importance of good manifest file settings, the use of relative paths, hash routing, and how to use I...',
   },
   {
     id: '3',
-    link: '/docs/cli/domains',
+    link: '/docs/cli/domains/',
     title: 'Adding Domains through Fleek CLI',
     description:
       'This guide takes you step by step through the process of providing a unique hostname, and configuring domain settings. It also introduces...',
   },
   {
     id: '4',
-    link: '/references/faq/my-links-arent-resolving',
+    link: '/references/faq/my-links-arent-resolving/',
     title: "My links aren't resolving",
     description:
       "Explore Fleek's method for resolving IPFS link issues. This in-depth tutorial steers you through the essential steps, from creating a new Next.js App to adding a custom domain, and finally optimizing y...",
   },
   {
     id: '5',
-    link: '/guides/fleek-templates-guide',
+    link: '/guides/fleek-templates-guide/',
     title: 'Deploying with Fleek Templates: Step-by-Step Guide',
     description:
       'With our ready-to-use templates, boilerplates, and the new Fleek CLI, deploying your first project to IPFS becomes a lightning-fast process. Turn building apps from the group up into a three-step process: Fork, Customize, and Deploy...',
