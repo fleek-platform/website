9ff81deb7a65da0b5487c0f38e7c3d7f097037ad
Kanishk Khurana <kanishkkhurana152@gmail.com>
2024-11-09 13:36:37 +0700

    chore:formatting fixes

--- src/components/DocIntroCard.tsx
+++ src/components/DocIntroCard.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 type DocIntroCardProps = {
   title: string;
   description?: string;
@@ -15,17 +15,19 @@ const DocIntroCard: React.FC<DocIntroCardProps> = ({
   return (
     <a
       href={href}
-      className="flex items-center justify-between rounded-12 bg-gray-dark-3 p-16 !text-gray-dark-11 no-underline ring-gray-dark-8 hover:cursor-pointer hover:bg-gray-dark-4 active:bg-gray-dark-3"
+      className="BigDocIntroCard relative flex items-center justify-between rounded-12 bg-gray-dark-3 p-16 !text-gray-dark-11 no-underline ring-gray-dark-8 hover:cursor-pointer hover:bg-gray-dark-4 active:bg-gray-dark-3"
     >
       <div className="flex w-full gap-5">
-        <div className="mx-5 rounded-12 bg-yellow p-1" />
+        <div className={`SmallDocIntroCard mx-5 rounded-12 p-1`} />
         <div className="w-full">
-          <div className="flex w-full items-center justify-between">
+          <div className="relative flex w-full items-center justify-between">
             <div className="typo-btn-action text-white">{title}</div>
-            {icon && (
+            {icon ? (
               <div>
                 <img src={icon} alt={description} className="w-20 " />
               </div>
+            ) : (
+              <div className="arrow absolute right-0">→</div>
             )}
           </div>
           {description && (
