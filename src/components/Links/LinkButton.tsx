873fcd3464349a7de5d7f467dfbf07cc0b60c14c
Bruno Camarneiro <camarneirobruno@gmail.com>
2024-11-26 12:40:33 +0100

    feat(PLAT-1840): :lipstick: Improve responsive design of Links page

--- src/components/Links/LinkButton.tsx
+++ src/components/Links/LinkButton.tsx
@@ -1,29 +1,36 @@
-import { Button } from '@components/Button';
+import clsx from 'clsx';
 import { FaArrowUp } from 'react-icons/fa6';
+import { Button } from '@components/Button';
+import { Target } from '@components/Link';
 
 type LinkButtonProps = {
   href: string;
   ariaLabel: string;
   label: string;
+  className?: string;
 };
 
 export const LinkButton: React.FC<LinkButtonProps> = ({
   href,
   ariaLabel,
   label,
+  className,
 }) => {
   return (
-    <Button variant="secondary" className="w-full py-7 font-normal" size="sm">
-      <a
-        aria-label={ariaLabel}
-        href={href}
-        rel="noopener noreferrer"
-        target="_blank"
-        className="flex w-full flex-row items-center justify-between"
-      >
-        <p className="text-white">{label}</p>
-        <FaArrowUp className="rotate-45" />
-      </a>
+    <Button
+      variant="secondary"
+      className={clsx(
+        'flex w-full flex-row items-center justify-between py-7 font-normal',
+        className,
+      )}
+      size="sm"
+      aria-label={ariaLabel}
+      href={href}
+      rel="noopener noreferrer"
+      target={Target.Blank}
+    >
+      <p className="text-white">{label}</p>
+      <FaArrowUp className="rotate-45" />
     </Button>
   );
 };
