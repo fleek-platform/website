8a9c2940a66b09147a5093eed02050b261696b9f
nemanjafleek <nemanjafleek@gmail.com>
2025-01-13 11:30:48 +0100

    refactor: add initial interface for the agents-ui package, extract it in AgentsUI folder

---
+++ src/components/AgentsUI/index.tsx
@@ -0,0 +1,36 @@
+import { AuthProvider } from '@components/AuthProvider/AuthProvider.tsx';
+import { useAuthentication } from '@components/AuthProvider/useAuthentication.ts';
+import {
+  getPlans,
+  getSubscriptions,
+} from '@components/AuthProvider/api/api.ts';
+
+// Import local implementation for now.
+// In the future will import package implementation with the same interface.
+import ElizaIntegrationLayer from '../Eliza/ElizaIntegrationLayer.tsx';
+
+export const AgentsUIIntegration: React.FC = () => {
+  const { isLoggedIn, isLoggingIn, login, activeProjectId, fetchFleekToken } =
+    useAuthentication();
+
+  return (
+    <ElizaIntegrationLayer
+      isLoggedIn={isLoggedIn}
+      isLoggingIn={isLoggingIn}
+      login={login}
+      activeProjectId={activeProjectId}
+      fetchFleekToken={fetchFleekToken}
+      getSubscriptions={getSubscriptions}
+      getPlans={getPlans}
+    />
+  );
+};
+
+const AgentsUI: React.FC = () => (
+  <AuthProvider>
+    <AgentsUIIntegration />
+  </AuthProvider>
+);
+
+// to be used in Astro
+export default AgentsUI;
