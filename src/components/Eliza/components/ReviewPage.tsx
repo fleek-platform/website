c8c6ee07b67038568c00e44aeeb6b16497a1c50e
Victor F. Santos <victor.gaard@gmail.com>
2025-01-22 13:30:41 -0300

    chore: revert line break

--- src/components/Eliza/components/ReviewPage.tsx
+++ src/components/Eliza/components/ReviewPage.tsx
@@ -114,7 +114,6 @@ export const ReviewPage: React.FC<ReviewPageProps> = ({
     if (Array.isArray(validated)) {
       return setErrors({ ...errors, form: validated });
     }
-
     const payload = JSON.stringify(parsedCharacterFile);
     onDeployBtnClick(payload);
   };
