77215d88db4225f1dbf47b9996be0cc1d65ac1a5
nemanjafleek <nemanjafleek@gmail.com>
2025-01-27 14:29:09 +0100

    feat: Hero section, basic styles, work in progress

--- src/components/Affiliates/Section.tsx
+++ src/components/Affiliates/Section.tsx
@@ -1,15 +1,35 @@
 import { cn } from '@utils/cn';
+import type { ReactNode, FC } from 'react';
 
 interface SectionProps {
-  title: string;
+  title: ReactNode;
+  isH1Title?: boolean;
+  subTitle?: string;
   className?: string;
-  children?: React.ReactNode;
+  children?: ReactNode;
 }
 
-const Section: React.FC<SectionProps> = ({ children, title, className }) => {
+const Section: FC<SectionProps> = ({
+  children,
+  title,
+  isH1Title = false,
+  subTitle,
+  className,
+}) => {
   return (
-    <div className={cn('', className)}>
-      <h1 className="text-38">{title}</h1>
+    <div className={cn('px-24 py-128 text-center md:px-32', className)}>
+      {isH1Title ? (
+        <h1 className="tracking-tight mb-24 text-36 font-bold leading-40 text-white md:text-60 md:leading-60">
+          {title}
+        </h1>
+      ) : (
+        <h2 className="text-30 md:text-36">{title}</h2>
+      )}
+      {subTitle && (
+        <p className="mx-auto mb-32 max-w-[672px] text-18 font-medium text-neutral-11 md:text-20">
+          {subTitle}
+        </p>
+      )}
       {children && <div className="text-16">{children}</div>}
     </div>
   );
--- src/components/Affiliates/index.tsx
+++ src/components/Affiliates/index.tsx
@@ -1,10 +1,32 @@
 import settings from '@base/settings.json';
 import Section from './Section';
+import { Button } from '@components/Button';
+import { Target } from '@components/Link';
 
 const AboutModule = {
   Hero: () => (
-    <Section title={settings.affiliatesPage.hero.title}>
-      <div className="">Hero section</div>
+    <Section
+      title={
+        <>
+          {settings.affiliatesPage.hero.titleWhite}
+          <span className="bg-gradient-to-r from-yellow-dark-9 to-yellow-dark-12 bg-clip-text text-transparent">
+            {settings.affiliatesPage.hero.titleYellow}
+          </span>
+        </>
+      }
+      isH1Title
+      subTitle={settings.affiliatesPage.hero.subTitle}
+    >
+      <Button
+        variant="primary"
+        className="inline-flex h-44 w-full max-w-[350px] rounded-6 px-32"
+        aria-label={settings.affiliatesPage.hero.ctaText}
+        href="https://app.fleek.xyz/"
+        rel="noopener noreferrer"
+        target={Target.Blank}
+      >
+        {settings.affiliatesPage.hero.ctaText}
+      </Button>
     </Section>
   ),
   WhyBecome: () => (
