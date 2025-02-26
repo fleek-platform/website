e0a3f3e68ca84096a6f33f0a4c823be032877fba
Helder Oliveira <heldrida@gmail.com>
2025-02-26 13:19:45 +0000

    fix: 🐛 linter

---
+++ src/components/Newsletter/index.tsx
@@ -0,0 +1,8 @@
+import { NewsletterSubscribeModal } from '@fleek-platform/agents-ui';
+
+export default () => (
+  <>
+    <h1>TODO: Remove this line (Newsletter test)</h1>
+    <NewsletterSubscribeModal open={true} />
+  </>
+);
