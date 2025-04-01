ac0d704644d56bcfffc07877f86db69893456672
Alen Ajam <alen@fleek.xyz>
2025-04-01 17:52:13 +0200

    feat: add video, three steps

---
+++ src/components/LandingPage/AgentsVideo.tsx
@@ -0,0 +1,9 @@
+export const AgentsVideo = () => {
+	return (
+		<img
+			src="/images/agents-video-preview.jpg"
+			alt="Agents video"
+			className="max-w-[800px] my-100 mx-auto rounded-10"
+		/>
+	);
+};
