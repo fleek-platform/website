154c8ed85a7702519d199c8d25f3b3d199a9a4bf
Victor F. Santos <victor.gaard@gmail.com>
2025-04-02 15:55:36 -0300

    chore: feedback

--- src/components/LandingPage/CaseStudies.tsx
+++ src/components/LandingPage/CaseStudies.tsx
@@ -8,27 +8,29 @@ import { Target } from '@components/Link';
 
 export const CaseStudies: React.FC = () => {
   return (
-    <div className="mx-auto flex max-w-[800px] flex-col items-center gap-24 px-24 py-100 text-center sm:text-left">
+    <div className="mx-auto flex flex-col items-center gap-24 px-24 py-100 sm:text-left">
       <Badge>
         <IoBook className="size-16" />
         Case studies
       </Badge>
-      <Text className="text-center">
-        How companies are using Fleek
-        <br />
-        <span className="text-yellow-dark-11">to boost their business</span>
+      <Text className="max-w-[580px] text-center">
+        How companies are using Fleek to{' '}
+        <span className="text-yellow-dark-11">boost their business</span>
       </Text>
       <Text variant="description" className="font-normal">
         Join others in making the agents to ease your life.
       </Text>
-      <div className="grid max-w-[800px] grid-cols-3 gap-24 pt-24">
-        <div className="flex flex-col justify-between rounded-8 border border-gray-dark-6 p-16">
+      <div className="grid w-full max-w-[800px] gap-24 pt-24 md:grid-cols-3">
+        <div className="flex flex-col justify-between gap-24 rounded-8 border border-gray-dark-6 p-16">
           <div className="flex flex-col gap-8">
             <Text variant="subtitle">We're enterprise-ready</Text>
             <Text variant="paragraph">
               Lots of businesses are jumping on board with Fleek for their
-              agents. We'd love to hear how you're planning to use it, so feel
-              free to reach out!
+              agents.
+              <br />
+              <br />
+              We'd love to hear how you're planning to use it, so feel free to
+              reach out!
             </Text>
           </div>
           <Button
@@ -40,10 +42,10 @@ export const CaseStudies: React.FC = () => {
             Contact sales
           </Button>
         </div>
-        <div className="col-span-2 flex flex-col gap-24">
+        <div className="flex flex-col gap-24 md:col-span-2">
           <div className="flex rounded-8 border border-gray-dark-6 bg-gray-dark-1">
-            <div className="flex flex-col justify-between gap-14 p-16">
-              <div className="flex flex-col">
+            <div className="flex flex-col justify-between p-16">
+              <div className="flex flex-col gap-8">
                 <Text variant="subtitle">ElizaOS</Text>
                 <Text variant="paragraph">
                   ElizaOS uses Fleek to improve their agent launchpad for over
@@ -51,7 +53,7 @@ export const CaseStudies: React.FC = () => {
                 </Text>
               </div>
               <Button variant="secondary" size="sm" className="mr-auto">
-                Read the case study <IoArrowForward />
+                Read the case study <IoArrowForward className="size-16" />
               </Button>
             </div>
             <img
@@ -62,8 +64,8 @@ export const CaseStudies: React.FC = () => {
             />
           </div>
           <div className="flex rounded-8 border border-gray-dark-6 bg-gray-dark-1">
-            <div className="flex flex-col justify-between gap-14 p-16">
-              <div className="flex flex-col">
+            <div className="flex flex-col justify-between p-16">
+              <div className="flex flex-col gap-8">
                 <Text variant="subtitle">Venice</Text>
                 <Text variant="paragraph">
                   Using Fleek to protect AI agents' data integrity and
@@ -71,7 +73,7 @@ export const CaseStudies: React.FC = () => {
                 </Text>
               </div>
               <Button variant="secondary" size="sm" className="mr-auto">
-                Read the case study <IoArrowForward />
+                Read the case study <IoArrowForward className="size-16" />
               </Button>
             </div>
             <img
