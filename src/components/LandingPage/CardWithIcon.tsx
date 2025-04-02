2487d00d6a492c771810d02834f3c3a440d877cd
Victor F. Santos <victor.gaard@gmail.com>
2025-04-02 12:40:43 -0300

    feat: security section, reusable components (#606)
    ## Why?
    <img width="1075" alt="image"
    src="https://github.com/user-attachments/assets/28efff9b-6d9d-4bae-8c40-3a097cffd2dd"
    />
    
    
    ## How?
    
    - Done A (replace with a breakdown of the steps)
    - Done B
    - Done C
    
    ## Tickets?
    
    - [Ticket 1](the-ticket-url-here)
    - [Ticket 2](the-ticket-url-here)
    - [Ticket 3](the-ticket-url-here)
    
    ## Contribution checklist?
    
    - [ ] The commit messages are detailed
    - [ ] The `build` command runs locally
    - [ ] Assets or static content are linked and stored in the project
    - [ ] Document filename is named after the slug
    - [ ] You've reviewed spelling using a grammar checker
    - [ ] For documentation, guides or references, you've tested the
    commands and steps
    - [ ] You've done enough research before writing
    
    ## Security checklist?
    
    - [ ] Sensitive data has been identified and is being protected properly
    - [ ] Injection has been prevented (parameterized queries, no eval or
    system calls)
    - [ ] The Components are escaping output (to prevent XSS)
    
    ## References?
    
    Optionally, provide references such as links
    
    ## Preview?
    
    Optionally, provide the preview url here

---
+++ src/components/LandingPage/CardWithIcon.tsx
@@ -0,0 +1,21 @@
+import type { IconType } from 'react-icons/lib';
+
+export const CardWithIcon = ({
+  title,
+  description,
+  icon,
+}: {
+  title: string;
+  description: string;
+  icon: IconType;
+}) => {
+  const IconComp = icon;
+  return (
+    <div className="relative flex flex-col overflow-hidden rounded-8 border border-neutral-6 bg-neutral-1 p-12">
+      <IconComp className="absolute -top-14 right-0 size-76 text-neutral-2" />
+      <IconComp className="size-34" />
+      <p className="mt-12 text-18 font-bold text-neutral-12">{title}</p>
+      <p className="mt-4">{description}</p>
+    </div>
+  );
+};
