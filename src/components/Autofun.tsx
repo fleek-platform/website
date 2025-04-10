7b131b1c50db91379c72e87e6bc49d0c5f9506ea
Victor F. Santos <victor.gaard@gmail.com>
2025-04-10 15:09:06 -0300

    chore: copy

--- src/components/Autofun.tsx
+++ src/components/Autofun.tsx
@@ -1,28 +1,75 @@
-// http://localhost:4321/autofun?autofun_token_id=23423sdf&referral=auto.fun
-
-import type { PropsWithChildren } from 'react';
+import { useEffect, type PropsWithChildren } from 'react';
 import type React from 'react';
+import { Text } from './LandingPage/Text';
+import { useLoginWithAgentsCTA } from '@hooks/useLoginWithAgentsCTA';
+
+const AUTOFUN_KEY = 'autofun_token_id';
 
 const Layout: React.FC<PropsWithChildren> = ({ children }) => {
   return (
-    <div className="flex h-full flex-col items-center justify-center font-plex-sans text-14 text-gray-dark-11">
+    <div className="flex h-full w-full max-w-[600px] flex-col justify-center gap-36 justify-self-center px-24 font-plex-sans text-14 text-gray-dark-11">
+      <img
+        src="images/autofun/logo.svg"
+        alt="Auto.fun logo"
+        width={154}
+        height={77}
+      />
       {children}
     </div>
   );
 };
 
 export const AutofunHero: React.FC = () => {
-  const params = new URLSearchParams(window.location.search);
-  const tokenId = params.get('autofun_token_id');
-  const referral = params.get('referral');
+  const { onDeployAgentCTA } = useLoginWithAgentsCTA();
 
-  if (!tokenId || !referral)
+  const params = new URLSearchParams(window.location.search);
+  const localStorageTokenId = localStorage.getItem(AUTOFUN_KEY);
+  const tokenId = params.get(AUTOFUN_KEY) || localStorageTokenId;
+
+  useEffect(() => {
+    if (localStorageTokenId || !tokenId) return;
+
+    localStorage.setItem(AUTOFUN_KEY, tokenId);
+  }, [localStorageTokenId, tokenId]);
+
+  if (!tokenId)
     return (
       <Layout>
-        Sorry, there's no autofun token id here. Please request the URL again.
+        <div className="space-y-12">
+          <Text
+            variant="subtitle"
+            as="h1"
+            className="text-[2.6rem] font-medium"
+          >
+            Invalid auto.fun token!
+          </Text>
+          <Text variant="paragraph" className="text-[1.6rem]">
+            Please request the correct URL from auto.fun again.
+          </Text>
+        </div>
       </Layout>
     );
 
-  console.log(tokenId, referral);
-  return <Layout>Hello world</Layout>;
+  return (
+    <Layout>
+      <div className="space-y-12">
+        <Text variant="subtitle" as="h1" className="text-[2.6rem] font-medium">
+          Welcome, auto.fun user!
+        </Text>
+        <Text variant="paragraph" className="text-[1.6rem]">
+          Fleek helps you create and manage AI agents, starting at $10/month
+          with a 7-day free trial. After creating your agent, you'll be
+          redirected to auto.fun. You can return to Fleek at any time to manage
+          your agents.
+        </Text>
+      </div>
+      <button
+        type="button"
+        onClick={onDeployAgentCTA}
+        className="mr-auto flex h-40 items-center whitespace-nowrap bg-[#04FF24] px-12 text-16 font-medium text-black"
+      >
+        Create your agent
+      </button>
+    </Layout>
+  );
 };
