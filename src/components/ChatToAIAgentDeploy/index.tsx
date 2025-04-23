b42b0851a8b079d409dda9f2c48e02d011a0ae09
Alen Ajam <alen@fleek.xyz>
2025-04-23 20:13:56 +0200

    Revert "Revert "feat: 🎸 Conversational based user journey (#615)""
    This reverts commit 290301eccf2e1549e4b922f6215da3ae77f22868.

---
+++ src/components/ChatToAIAgentDeploy/index.tsx
@@ -0,0 +1,95 @@
+import '@fleek-platform/agents-ui/styles';
+import {
+  ChatBox,
+  type FileWithPreview,
+  DRAFT_BOOTSTRAP_DATA_KEY,
+  ROUTE_NEW_DRAFT,
+} from '@fleek-platform/agents-ui';
+import { useAuthStore } from '@fleek-platform/login-button';
+import { storeFunnelData } from '@utils/funnel';
+import { fileToBase64 } from '@utils/file';
+import { setReferralQueryKeyValuePair } from '@utils/referrals';
+
+const MAX_FILE_SIZE = 10 * 1024 * 1024;
+
+export const ChatToAIAgentDeploy = ({
+  role,
+  onDescriptionChange,
+}: {
+  role?: string;
+  onDescriptionChange?: () => void;
+}) => {
+  const { triggerLoginModal, isLoggedIn } = useAuthStore();
+
+  const onSubmit = async (description: string, files: FileWithPreview[]) => {
+    console.log('[debug] Description:', description);
+    console.log('[debug] Files:', files);
+
+    // TODO: Validate data
+
+    const fileDataPromises = files.map((file) => fileToBase64(file));
+    const fileDataArray = await Promise.all(fileDataPromises);
+
+    const data = {
+      mode: 'chat',
+      fromApp: 'website',
+      prompt: description,
+      timestamp: new Date(),
+    };
+
+    setReferralQueryKeyValuePair('agents');
+    storeFunnelData({
+      key: DRAFT_BOOTSTRAP_DATA_KEY,
+      data,
+    });
+
+    if (isLoggedIn) {
+      const currentParams = new URLSearchParams(window.location.search);
+
+      const targetUrl = new URL(
+        `${import.meta.env.PUBLIC_UI_AGENTS_APP_URL}${ROUTE_NEW_DRAFT}`,
+      );
+
+      currentParams.forEach((value, key) => {
+        targetUrl.searchParams.append(key, value);
+      });
+
+      window.location.assign(targetUrl.toString());
+
+      return true;
+    }
+
+    if (typeof triggerLoginModal !== 'function') {
+      console.log('[debug] triggerLoginModal is not a fn!');
+
+      return false;
+    }
+
+    triggerLoginModal(true);
+
+    return true;
+  };
+
+  const onSuccess = () => {
+    console.log('[debug] Submission successful');
+  };
+
+  const onError = (error: Error) => {
+    console.error('[debug] Submission failed:', error);
+  };
+
+  return (
+    <div className="agents-ui my-20 flex justify-center text-14">
+      <ChatBox
+        onSubmit={onSubmit}
+        onSuccess={onSuccess}
+        onError={onError}
+        maxFileSize={MAX_FILE_SIZE}
+        onDescriptionChange={onDescriptionChange}
+        prompt={
+          role ? `I want to create a ${role.toLocaleLowerCase()}.` : undefined
+        }
+      />
+    </div>
+  );
+};
--- src/components/LandingPage/Hero.tsx
+++ src/components/LandingPage/Hero.tsx
@@ -1,71 +1,127 @@
 import settings from '@base/settings.json';
-import { FaChevronRight } from 'react-icons/fa6';
 import { Announcement } from '../Announcement';
 import { Button } from '../Button';
 import { Text } from './Text';
 import { BlurFade } from './BlurFade';
-import { useLoginWithAgentsCTA } from '@hooks/useLoginWithAgentsCTA';
+import {
+  IoHeartOutline,
+  IoLaptopOutline,
+  IoShirtOutline,
+  IoAirplaneOutline,
+  IoHappyOutline,
+  IoHeadsetOutline,
+  IoPawOutline,
+} from 'react-icons/io5';
+import { ChatToAIAgentDeploy } from '@components/ChatToAIAgentDeploy';
+import type { IconType } from 'react-icons/lib';
+import { cn } from '@utils/cn';
+import { useState } from 'react';
 
 const calculateDelay = (factor: number) => 0.25 * factor;
 
+type Template = {
+  name: string;
+  icon: IconType;
+  iconClass?: string;
+};
+
+const templates: Template[] = [
+  {
+    name: 'Tech Reviewer',
+    icon: IoLaptopOutline,
+    iconClass: 'text-[#768293]',
+  },
+  {
+    name: 'Fashion Stylist',
+    icon: IoShirtOutline,
+    iconClass: 'text-[#D98F66]',
+  },
+  {
+    name: 'Travel Guide',
+    icon: IoAirplaneOutline,
+    iconClass: 'text-[#8A75AD]',
+  },
+  {
+    name: 'Comedian',
+    icon: IoHappyOutline,
+    iconClass: 'text-[#C4A06B]',
+  },
+  {
+    name: 'Pet Enthusiast',
+    icon: IoPawOutline,
+    iconClass: 'text-[#759E9A]',
+  },
+  {
+    name: 'Musician',
+    icon: IoHeadsetOutline,
+    iconClass: 'text-[#6E927D]',
+  },
+  {
+    name: 'Relationship Coach',
+    icon: IoHeartOutline,
+    iconClass: 'text-[#C07DA5]',
+  },
+];
+
 export const Hero = () => {
-  const { onDeployAgentCTA } = useLoginWithAgentsCTA();
+  const [role, setRole] = useState<string>();
 
   return (
-    <header className="relative mx-auto w-full max-w-[1048px] px-24">
-      <div className="pointer-events-none absolute -left-320 -top-1/2 -z-1 h-800 w-800 bg-[radial-gradient(closest-side,rgb(34_34_34_/0.85),transparent)] sm:-left-304 sm:-top-240 sm:h-400" />
-      <section className="flex flex-col justify-center gap-24 pt-64">
-        {settings.site.announcementMarquee.visible && (
-          <BlurFade delay={calculateDelay(0)} className="w-fit max-w-full">
-            <Announcement variant="content" />
-          </BlurFade>
-        )}
-        <section className="flex max-w-800 flex-col gap-24 pt-12">
-          <BlurFade delay={calculateDelay(1)}>
-            <Text as="h1">{settings.landingPage.hero.h1}</Text>
-          </BlurFade>
-          <BlurFade delay={calculateDelay(2)}>
-            <Text variant="description" as="h2">
-              {settings.landingPage.hero.h2}
-            </Text>
-          </BlurFade>
-          <div className="flex items-center gap-12">
+    <header className="relative mx-auto w-full max-w-[1048px] px-24 pt-42 sm:pt-80">
+      <div className="flex flex-col justify-center pt-64 text-center">
+        <div className="flex flex-col gap-48">
+          <div className="flex flex-col gap-36">
+            <div className="flex flex-col gap-24">
+              {settings.site.announcementMarquee.visible && (
+                <BlurFade
+                  delay={calculateDelay(0)}
+                  className="w-fit max-w-full self-center"
+                >
+                  <Announcement variant="content" />
+                </BlurFade>
+              )}
+              <div className="flex max-w-800 flex-col gap-24 self-center pt-12">
+                <BlurFade delay={calculateDelay(1)}>
+                  <h1 className="text-52 font-medium leading-none text-neutral-12">
+                    {settings.landingPage.hero.h1}
+                  </h1>
+                </BlurFade>
+                <BlurFade delay={calculateDelay(2)}>
+                  <Text variant="description" as="h2" className="font-normal">
+                    {settings.landingPage.hero.h2}
+                  </Text>
+                </BlurFade>
+              </div>
+            </div>
             <BlurFade delay={calculateDelay(3)}>
-              <Button href="#" onClick={onDeployAgentCTA}>
-                {settings.landingPage.hero.primaryCta}
-              </Button>
-            </BlurFade>
-            <BlurFade delay={calculateDelay(3.5)}>
-              <Button variant="ghost" href="/docs/">
-                {settings.landingPage.hero.secondaryCta}
-                <FaChevronRight className="size-12" />
-              </Button>
+              <ChatToAIAgentDeploy
+                role={role}
+                onDescriptionChange={() => setRole(undefined)}
+              />
             </BlurFade>
           </div>
-        </section>
-        <div className="relative h-[400px]  lg:h-[730px]">
-          <BlurFade delay={calculateDelay(6.5)}>
-            <div className="pointer-events-none absolute -top-152 left-[12%] h-496 w-608 bg-[radial-gradient(closest-side,rgb(34_34_34_/0.85),transparent)]"></div>
+          <BlurFade delay={calculateDelay(4)}>
+            <div className="mx-auto flex max-w-[600px] flex-wrap justify-center gap-12">
+              {templates.map((item, index) => {
+                const Icon = item.icon;
+                return (
+                  <Button
+                    // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+                    key={index}
+                    variant="secondary-ghost"
+                    size="sm"
+                    className="shrink-0 gap-8"
+                    onClick={() => setRole(item.name)}
+                  >
+                    <Icon className={cn('size-16', item.iconClass)} />
+                    {item.name}
+                  </Button>
+                );
+              })}
+            </div>
           </BlurFade>
-          <div className="absolute inset-0 -left-[4%] top-60 h-400 w-[540px] origin-top-left overflow-hidden rounded-14 border border-r-0 border-gray-dark-3 bg-gray-dark-1/50 transition-all [mask-image:linear-gradient(to_bottom,rgba(0,0,0,1),rgba(0,0,0,1),rgba(0,0,0,0))] [transform:rotateX(35deg)_rotateY(7deg)_rotate(-14.337deg)] md:w-[800px] lg:-left-[11%] lg:top-108 lg:h-[800px] lg:w-[1200px] xl:w-[1300px] 2xl:-left-[20%] 2xl:w-[1452px]">
-            <BlurFade delay={calculateDelay(6)}>
-              <picture>
-                <source
-                  srcSet={settings.landingPage.hero.imageMobile}
-                  media="(max-width: 600px)"
-                />
-                <img
-                  src={settings.landingPage.hero.imageDesktop}
-                  alt="Fleek dashboard"
-                  width="100%"
-                  height="100%"
-                />
-              </picture>
-            </BlurFade>
-            <div className="pointer-events-none absolute bottom-0 right-0 top-0 w-400 bg-gradient-to-l from-black via-transparent to-transparent" />
-          </div>
         </div>
-      </section>
+      </div>
     </header>
   );
 };
