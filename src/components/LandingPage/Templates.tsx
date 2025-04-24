fe3b539a695a2beab6d03db104226e62a14a517b
Victor F. Santos <victor.gaard@gmail.com>
2025-04-23 21:07:06 -0300

    feat: explore section

--- src/components/LandingPage/Templates.tsx
+++ src/components/LandingPage/Templates.tsx
@@ -1,107 +1,90 @@
-import { Button } from '@components/Button';
-import {
-  IoBriefcase,
-  IoBulb,
-  IoCopy,
-  IoGrid,
-  IoHappy,
-  IoHelpCircle,
-  IoStatsChart,
-} from 'react-icons/io5';
-import type { IconType } from 'react-icons/lib';
 import 'keen-slider/keen-slider.min.css';
 import { useKeenSlider } from 'keen-slider/react';
 import { Badge } from './Badge';
 import { Text } from './Text';
-
-const categories = [
-  'FUN',
-  'RESEARCH',
-  'MARKETING',
-  'SUPPORT',
-  'BUSINESS',
-] as const;
-
-type Category = (typeof categories)[number];
-
-const categoriesMap: Record<
-  Category,
-  {
-    label: string;
-    icon: IconType;
-  }
-> = {
-  FUN: {
-    label: 'Fun',
-    icon: IoHappy,
-  },
-  RESEARCH: {
-    label: 'Research',
-    icon: IoBulb,
-  },
-  MARKETING: {
-    label: 'Marketing',
-    icon: IoStatsChart,
-  },
-  SUPPORT: {
-    label: 'Support',
-    icon: IoHelpCircle,
-  },
-  BUSINESS: {
-    label: 'Business',
-    icon: IoBriefcase,
-  },
-};
+import { IoChatbubbleEllipsesOutline } from 'react-icons/io5';
 
 type Template = {
   name: string;
-  description: string;
-  integrations: number;
+  category: string;
   image: string;
+  author: string;
+  socials: {
+    name: string;
+    logo: string;
+    followers: number;
+  }[];
 };
 
 const templates: Template[] = [
   {
-    name: 'Travel Guide 1',
-    description: 'Can get you the best sposts to go.',
-    integrations: 3,
-    image: '/images/agent-avatar.png',
+    name: 'Jocelyn',
+    category: 'Companion',
+    image: '/images/landing-page/templates/template-1.png',
+    author: 'Fleek',
+    socials: [
+      {
+        name: 'twitter',
+        logo: '/images/landing-page/templates/socials/x.svg',
+        followers: 56600,
+      },
+      {
+        name: 'instagram',
+        logo: '/images/landing-page/templates/socials/instagram.svg',
+        followers: 98200,
+      },
+      {
+        name: 'tiktok',
+        logo: '/images/landing-page/templates/socials/tiktok.svg',
+        followers: 85300,
+      },
+    ],
   },
   {
-    name: 'Travel Guide 2',
-    description: 'Can get you the best sposts to go.',
-    integrations: 3,
-    image: '/images/agent-avatar.png',
+    name: 'Crypto Kyle',
+    category: 'Crypto',
+    image: '/images/landing-page/templates/template-2.png',
+    author: 'Fleek',
+    socials: [
+      {
+        name: 'twitter',
+        logo: '/images/landing-page/templates/socials/x.svg',
+        followers: 112400,
+      },
+      {
+        name: 'instagram',
+        logo: '/images/landing-page/templates/socials/instagram.svg',
+        followers: 2400,
+      },
+      {
+        name: 'tiktok',
+        logo: '/images/landing-page/templates/socials/tiktok.svg',
+        followers: 1100,
+      },
+    ],
   },
   {
-    name: 'Travel Guide 3',
-    description: 'Can get you the best sposts to go.',
-    integrations: 3,
-    image: '/images/agent-avatar.png',
-  },
-  {
-    name: 'Travel Guide 4',
-    description: 'Can get you the best sposts to go.',
-    integrations: 3,
-    image: '/images/agent-avatar.png',
-  },
-  {
-    name: 'Travel Guide 5',
-    description: 'Can get you the best sposts to go.',
-    integrations: 3,
-    image: '/images/agent-avatar.png',
-  },
-  {
-    name: 'Travel Guide 6',
-    description: 'Can get you the best sposts to go.',
-    integrations: 3,
-    image: '/images/agent-avatar.png',
-  },
-  {
-    name: 'Travel Guide 7',
-    description: 'Can get you the best sposts to go.',
-    integrations: 3,
-    image: '/images/agent-avatar.png',
+    name: 'Kanye East',
+    category: 'Humor',
+    image: '/images/landing-page/templates/template-3.png',
+    author: 'Fleek',
+    socials: [
+      {
+        name: 'twitter',
+        logo: '/images/landing-page/templates/socials/x.svg',
+        followers: 1500,
+      },
+      {
+        name: 'instagram',
+        logo: '/images/landing-page/templates/socials/instagram.svg',
+        followers: 1100,
+      },
+      {
+        name: 'tiktok',
+        logo: '/images/landing-page/templates/socials/tiktok.svg',
+        followers: 659,
+      },
+    ],
   },
 ];
 
@@ -111,30 +94,30 @@ export const Templates = () => {
     mode: 'free-snap',
     slides: {
       spacing: 24,
-      perView: 1,
+      perView: 1.2,
     },
     breakpoints: {
       '(min-width: 640px)': {
         slides: {
-          perView: 2,
+          perView: 1.2,
           spacing: 24,
         },
       },
       '(min-width: 768px)': {
         slides: {
-          perView: 3,
+          perView: 1.5,
           spacing: 24,
         },
       },
       '(min-width: 1024px)': {
         slides: {
-          perView: 4,
+          perView: 2.2,
           spacing: 24,
         },
       },
       '(min-width: 1280px)': {
         slides: {
-          perView: 5,
+          perView: 2.45,
           spacing: 24,
         },
       },
@@ -144,29 +127,21 @@ export const Templates = () => {
   return (
     <div className="flex flex-col items-center px-24 py-48 text-center sm:py-100">
       <Badge>
-        <IoGrid className="size-16" />
-        Templates
+        <span>🌎</span> Explore
       </Badge>
-      <Text className="max-w-[600px] pt-24">
-        <span className="text-yellow">Get started</span> with an agent using our
-        expertly-crafted templates
+      <Text variant="subtitle" className="max-w-[600px] pt-24 text-[3.8rem]">
+        Who do you want to chat with?
       </Text>
       <p className="mt-24 text-18 text-neutral-11">
-        No need to start from scratch. Customize 100+ templates.
+        Explore social agents and start chatting.
       </p>
-
-      <div className="mt-24 flex flex-wrap justify-center gap-8">
-        {categories.map((item) => (
-          <CategoryButton key={item} category={item} />
-        ))}
-      </div>
 
       <div className="relative w-full overflow-hidden">
         {/* Left Fade */}
-        <div className="pointer-events-none absolute left-0 top-0 z-10 hidden h-full w-[20%] bg-gradient-to-r from-black to-transparent md:block" />
+        <div className="pointer-events-none absolute left-0 top-0 z-10 h-full w-[20%] bg-gradient-to-r from-neutral-1 to-transparent md:block" />
 
         {/* Right Fade */}
-        <div className="pointer-events-none absolute right-0 top-0 z-10 hidden h-full w-[20%] bg-gradient-to-l from-black to-transparent md:block" />
+        <div className="pointer-events-none absolute right-0 top-0 z-10 h-full w-[20%] bg-gradient-to-l from-neutral-1 to-transparent md:block" />
 
         <div
           ref={sliderRef}
@@ -174,7 +149,7 @@ export const Templates = () => {
         >
           {templates.map((template) => (
             <div key={template.name} className="keen-slider__slide">
-              <TemplateCard {...template} />
+              <TemplateCard template={template} />
             </div>
           ))}
         </div>
@@ -183,39 +158,56 @@ export const Templates = () => {
   );
 };
 
-export const CategoryButton = ({ category }: { category: Category }) => {
-  const obj = categoriesMap[category];
-  const Icon = obj.icon;
-  return (
-    <Button variant="secondary" size="sm">
-      <Icon className="mr-2 size-16" />
-      {obj.label}
-    </Button>
-  );
-};
+export const TemplateCard = ({ template }: { template: Template }) => {
+  function formatCompactNumber(value: number): string {
+    return new Intl.NumberFormat('en', {
+      notation: 'compact',
+      maximumFractionDigits: 1,
+    }).format(value);
+  }
 
-export const TemplateCard = ({
-  name,
-  description,
-  integrations,
-  image,
-}: {
-  name: string;
-  description: string;
-  integrations: number;
-  image: string;
-}) => {
   return (
-    <div className="flex flex-col rounded-8 border border-neutral-6 bg-neutral-1 p-12 text-start">
-      <img src={image} alt="Agent avatar" className="size-50" />
-      <p className="mt-12 text-18 font-medium text-neutral-12">{name}</p>
-      <p className="mt-4 text-12 text-neutral-11">{description}</p>
-      <div className="mt-20 flex items-center justify-between">
-        <p className="text-12">+ {integrations} integrations</p>
-        <Button size="sm" variant="app-primary">
-          <IoCopy className="size-16" />
-          Clone
-        </Button>
+    <div className="flex overflow-clip rounded-12 border border-neutral-6 bg-neutral-2">
+      <img
+        src={template.image}
+        alt="Agent avatar"
+        className="size-[173px] shrink-0 object-cover"
+      />
+      <div className="flex w-full flex-col items-start gap-10 px-12 py-16">
+        <div className="flex w-full items-start justify-between">
+          <div className="text-left">
+            <Text variant="subtitle">{template.name}</Text>
+            <Text variant="paragraph" className="text-[1.2rem]">
+              By <span className="text-neutral-12">{template.author}</span>
+            </Text>
+          </div>
+          <div className="rounded-full border border-neutral-6 bg-neutral-1 px-8 py-4 text-12 text-neutral-12">
+            {template.category}
+          </div>
+        </div>
+        <div className="grid w-full grid-cols-3 gap-6">
+          {template.socials.map((social) => (
+            <div
+              key={`${template.name} - ${social.name}`}
+              className="flex items-center justify-center gap-4 rounded-8 bg-gray-dark-4 p-8"
+            >
+              <img
+                src={social.logo}
+                alt="Social media logo"
+                className="size-18"
+              />
+              <Text variant="paragraph" className="font-medium text-neutral-12">
+                {formatCompactNumber(social.followers)}
+              </Text>
+            </div>
+          ))}
+        </div>
+        <button
+          type="button"
+          className="flex w-full cursor-not-allowed items-center justify-center gap-6 rounded-8 bg-white p-8 font-medium text-black"
+        >
+          <IoChatbubbleEllipsesOutline /> Coming soon to chat
+        </button>
       </div>
     </div>
   );
