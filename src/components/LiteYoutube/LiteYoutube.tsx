a5c09f7dd9306f5670564aeb4ada2407e235cb5b
Alen Ajam <alen@fleek.xyz>
2025-04-24 16:03:04 +0200

    feat(landing): replace local video with youtube video

--- src/components/LiteYoutube/LiteYoutube.tsx
+++ src/components/LiteYoutube/LiteYoutube.tsx
@@ -1,14 +1,16 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, type CSSProperties } from 'react';
 
 import 'lite-youtube-embed/src/lite-yt-embed.css';
 
 interface ILiteYouTubeProps {
   videoId: string;
   title?: string;
+  style?: CSSProperties;
 }
 const LiteYouTube: React.FC<ILiteYouTubeProps> = ({
   videoId,
   title = 'YouTube video',
+  style = {},
 }) => {
   const [isClient, setIsClient] = useState(false);
 
@@ -22,7 +24,7 @@ const LiteYouTube: React.FC<ILiteYouTubeProps> = ({
   return (
     <lite-youtube
       videoid={videoId}
-      style={{ width: '100%', aspectRatio: '16/9' }}
+      style={{ width: '100%', aspectRatio: '16/9', ...style }}
       params="modestbranding=1&rel=0"
       title={title}
     ></lite-youtube>
