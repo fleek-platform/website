6558c0f526fcdcd10c6178079efd7bd9e7d5c740
Alen Ajam <alen@fleek.xyz>
2025-04-24 16:52:11 +0200

    chore(landing): remove border top from pricing block

--- src/components/LandingPage/Container.tsx
+++ src/components/LandingPage/Container.tsx
@@ -21,13 +21,11 @@ export const Container: React.FC<ContainerProps> = ({
       )}
     >
       <div
-        className={cn(
-          'absolute top-0 -z-1 h-full w-1/3 from-gray-dark-2/80 via-transparent to-transparent',
-          {
-            'left-0 bg-gradient-to-br': gradient === 'left',
-            'right-0 bg-gradient-to-bl': gradient === 'right',
-          },
-        )}
+        className={cn('absolute top-0 -z-1 h-full w-1/3', {
+          'from-gray-dark-2/80 via-transparent to-transparent': gradient,
+          'left-0 bg-gradient-to-br': gradient === 'left',
+          'right-0 bg-gradient-to-bl': gradient === 'right',
+        })}
       />
       <div
         className={cn(
--- src/components/LandingPage/PricingLanding.tsx
+++ src/components/LandingPage/PricingLanding.tsx
@@ -1,18 +1,13 @@
 import type React from 'react';
-import { useState } from 'react';
 import { Container } from './Container';
 import { Text } from './Text';
 import { PricingPlanHero } from '@components/Pricing/PricingPlanHero';
 
 const PricingLanding: React.FC = () => {
-  const [priceFocus, setPriceFocus] = useState<'ai-agents' | 'hosting'>(
-    'ai-agents',
-  );
-
   return (
     <Container
-      gradient="right"
-      classNameOuterContainer="sm:pt-[75px] sm:pb-[51px]"
+      classNameOuterContainer="sm:pt-[75px] sm:pb-[51px] border-none"
+      gradient=""
     >
       <div className="pb-44 text-center">
         <Text
@@ -22,10 +17,8 @@ const PricingLanding: React.FC = () => {
           AI Agent Plans
         </Text>
 
-        <Text variant="description" className="mt-24 font-normal">
-          {priceFocus === 'ai-agents'
-            ? 'Create, manage and monetize AI agents using Fleek.'
-            : "Create, deploy and manage your Web apps on Fleek's platform."}
+        <Text variant="description" className="mt-24 font-inter font-normal">
+          Create, manage and monetize AI agents using Fleek.
         </Text>
       </div>
       <PricingPlanHero priceFocus="ai-agents" />
