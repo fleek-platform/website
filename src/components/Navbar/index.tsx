3f3c8ff36a25bf8a12d36a8816ba03b6071916da
Alen Ajam <alen@fleek.xyz>
2025-04-28 21:36:37 +0200

    feat: new funnel to immediately deploy agent (#662)
    ## Why?
    
    Clear and short explanation here.
    
    ## How?
    
    - Done A (replace with a breakdown of the steps)
    - Done B
    - Done C
    
    ## Tickets?
    
    -
    [PLAT-2796](https://linear.app/fleekxyz/issue/PLAT-2796/deploy-agent-after-first-prompt-in-chatbox)
    
    ## Contribution checklist?
    
    - [ ] The commit messages are detailed
    - [ ] The `build` command runs locally
    - [ ] Assets or static content are linked and stored in the project
    - [ ] Document filename is named after the slug
    - [ ] You've reviewed spelling using a grammar checker
    - [ ] For documentation, guides or references, you've tested the
    commands and steps
    - [ ] You've done enough research before writing
    
    ## Security checklist?
    
    - [ ] Sensitive data has been identified and is being protected properly
    - [ ] Injection has been prevented (parameterized queries, no eval or
    system calls)
    - [ ] The Components are escaping output (to prevent XSS)
    
    ## References?
    
    Optionally, provide references such as links
    
    ## Preview?
    
    Uploading Screen_Recording_2025-04-24_at_19.17.16.mov…

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -18,25 +18,19 @@ import type { Project } from '@fleek-platform/sdk/browser';
 import { isClient } from '../../utils/common';
 import { useSession } from '@hooks/useSession';
 import { isReferralName } from '@utils/referrals';
-import { ROUTE_NEW_DRAFT } from '@fleek-platform/agents-ui';
 
 const dashboardUrl = import.meta.env.PUBLIC_UI_APP_URL;
-const agentsUrl = `${import.meta.env.PUBLIC_UI_AGENTS_APP_URL}${ROUTE_NEW_DRAFT}`;
 
 const onAuthenticationSuccess = () => {
-  if (!isClient) return;
+  if (!isClient || isReferralName('agents')) return;
 
   const currentParams = new URLSearchParams(window.location.search);
 
-  let targetUrl = new URL(agentsUrl);
+  const targetUrl = new URL(dashboardUrl);
 
   currentParams.forEach((value, key) => {
     targetUrl.searchParams.append(key, value);
   });
-
-  if (isReferralName('dashboard')) {
-    targetUrl = new URL(dashboardUrl);
-  }
 
   window.location.assign(targetUrl.toString());
 };
