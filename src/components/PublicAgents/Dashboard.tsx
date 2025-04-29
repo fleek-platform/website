3be53ecbe9c4e0f830f0fd5693f59bd6c17621fe
Victor F. Santos <victor.gaard@gmail.com>
2025-04-29 17:52:00 -0300

    feat: typewriter effect and small delay to send agent msg

--- src/components/PublicAgents/Dashboard.tsx
+++ src/components/PublicAgents/Dashboard.tsx
@@ -31,7 +31,8 @@ import {
 } from 'react-icons/pi';
 import type React from 'react';
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import { animate } from 'motion';
 
 const Button: React.FC<
   React.PropsWithChildren & React.ButtonHTMLAttributes<HTMLButtonElement>
@@ -221,6 +222,64 @@ const Dropdown: React.FC<DropdownProps> = ({ agent }) => {
   );
 };
 
+const Typewriter: React.FC<{ text: string }> = ({ text }) => {
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const ref = useRef<HTMLParagraphElement>(null);
+
+  useEffect(() => {
+    if (hasLoaded) return;
+
+    setTimeout(() => setHasLoaded(true), 2000);
+  }, [hasLoaded]);
+
+  useEffect(() => {
+    if (!ref.current || !text || !hasLoaded) return;
+
+    const nodes = Array.from(ref.current.children) as HTMLElement[];
+    nodes.forEach((char, i) => {
+      animate(char, { opacity: [0, 1] }, { delay: i * 0.009, duration: 0.2 });
+    });
+  }, [text, hasLoaded]);
+
+  const splitGraphemes = (text: string): string[] => {
+    if ('Segmenter' in Intl) {
+      const segmenter = new Intl.Segmenter('en', { granularity: 'grapheme' });
+      return Array.from(segmenter.segment(text), (s) => s.segment);
+    }
+    return text.split('');
+  };
+
+  if (!hasLoaded)
+    return (
+      <div className="ml-6 mt-8 flex h-4 w-10 items-center justify-center">
+        <div className="flex space-x-4">
+          <div className="[animation-delay:-0.3s]' h-4 w-4 animate-bounce rounded-full bg-gray-dark-8" />
+          <div className="h-4 w-4 animate-bounce rounded-full bg-gray-dark-8 [animation-delay:-0.15s]" />
+          <div className="h-4 w-4 animate-bounce rounded-full bg-gray-dark-8" />
+        </div>
+      </div>
+    );
+
+  return (
+    <p
+      className="whitespace-pre-wrap font-medium leading-16 text-neutral-12"
+      ref={ref}
+    >
+      {splitGraphemes(text).map((char, i) => (
+        <span
+          key={`${i}: ${char}`}
+          style={{
+            opacity: 0,
+            display: 'inline-block',
+          }}
+        >
+          {char === ' ' ? '\u00A0' : char}
+        </span>
+      ))}
+    </p>
+  );
+};
+
 type ContentProps = {
   agent: PublicAgent;
 };
@@ -281,15 +340,13 @@ const Content: React.FC<ContentProps> = ({ agent }) => {
                 className="rounded-4"
               />
               <div className="flex flex-col gap-16">
-                <p className="font-medium leading-16 text-neutral-12">
-                  {greeting}
-                </p>
-                <div className="flex items-center gap-12">
+                <Typewriter text={greeting} />
+                {/* <div className="flex items-center gap-12">
                   <PiArrowClockwiseBold className="size-16" />
                   <PiCopyBold className="size-16" />
                   <PiThumbsUpBold className="size-16" />
                   <PiThumbsDownBold className="size-16" />
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
