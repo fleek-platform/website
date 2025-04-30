c067f04e796736027e8746154aaa62318b107c44
Alen Ajam <alen@fleek.xyz>
2025-04-30 11:13:12 +0200

    feat: chat to deploy (#681)
    ## Why?
    
    Clear and short explanation here.
    
    ## How?
    
    - Done A (replace with a breakdown of the steps)
    - Done B
    - Done C
    
    ## Tickets?
    
    -
    [PLAT-2796](https://linear.app/fleekxyz/issue/PLAT-2796/deploy-agent-after-first-prompt-in-chatbox)
    
    ## Contribution checklist?
    
    - [x] The commit messages are detailed
    - [x] The `build` command runs locally
    - [ ] Assets or static content are linked and stored in the project
    - [ ] Document filename is named after the slug
    - [ ] You've reviewed spelling using a grammar checker
    - [ ] For documentation, guides or references, you've tested the
    commands and steps
    - [ ] You've done enough research before writing
    
    ## Security checklist?
    
    - [ ] Sensitive data has been identified and is being protected properly
    - [ ] Injection has been prevented (parameterized queries, no eval or
    system calls)
    - [ ] The Components are escaping output (to prevent XSS)
    
    ## References?
    
    Optionally, provide references such as links
    
    ## Preview?
    
    existing user:
    
    
    https://github.com/user-attachments/assets/7714cbac-473f-4b31-8b77-172c399c717d
    
    ---
    
    new user:
    
    
    https://github.com/user-attachments/assets/892d1f76-ed34-44a3-a276-b302e387218b

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -18,30 +18,20 @@ import type { Project } from '@fleek-platform/sdk/browser';
 import { isClient } from '../../utils/common';
 import { useSession } from '@hooks/useSession';
 import { isReferralName } from '@utils/referrals';
-import { ROUTE_NEW_DRAFT } from '@fleek-platform/agents-ui';
 
 const dashboardUrl = import.meta.env.PUBLIC_UI_APP_URL;
 const agentsUrl = import.meta.env.PUBLIC_UI_AGENTS_APP_URL;
-const agentsNewDraftUrl = `${import.meta.env.PUBLIC_UI_AGENTS_APP_URL}${ROUTE_NEW_DRAFT}`;
 
 const onAuthenticationSuccess = () => {
-  if (!isClient) return;
+  if (!isClient || isReferralName('agents')) return;
 
   const currentParams = new URLSearchParams(window.location.search);
 
-  let targetUrl = new URL(agentsNewDraftUrl);
+  const targetUrl = new URL(agentsUrl);
 
   currentParams.forEach((value, key) => {
     targetUrl.searchParams.append(key, value);
   });
-
-  if (isReferralName('dashboard')) {
-    targetUrl = new URL(dashboardUrl);
-  }
-
-  if (isReferralName('agents')) {
-    targetUrl = new URL(agentsUrl);
-  }
 
   window.location.assign(targetUrl.toString());
 };
