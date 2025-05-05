37f90db4e4c6870deda3f563d833c3c9264ca966
Victor F. Santos <victor.gaard@gmail.com>
2025-05-05 16:42:13 -0300

    feat: templates section

--- src/components/LandingPage/Templates.tsx
+++ src/components/LandingPage/Templates.tsx
@@ -1,96 +1,12 @@
-import 'keen-slider/keen-slider.min.css';
-import { useKeenSlider, type KeenSliderPlugin } from 'keen-slider/react';
+import { PiChatCircleDotsBold } from 'react-icons/pi';
 import { Badge } from './Badge';
 import { Text } from './Text';
-import { IoChatbubbleEllipsesOutline } from 'react-icons/io5';
-import { useState } from 'react';
-import { cn } from '@utils/cn';
 import {
   publicAgents,
   type PublicAgent,
 } from '@components/PublicAgents/config';
 
-const ContinuousAutoscroll: KeenSliderPlugin = (slider) => {
-  let raf: number;
-  let last: number;
-  let paused = false;
-
-  const speed = 0.000025;
-
-  const animate = (time: number) => {
-    if (!last) {
-      last = time;
-      raf = requestAnimationFrame(animate);
-      return;
-    }
-
-    const dt = time - last;
-    last = time;
-
-    if (!paused) {
-      slider.track.to((slider.track.details?.position ?? 0) - speed * dt);
-    }
-
-    raf = requestAnimationFrame(animate);
-  };
-
-  slider.on('created', () => {
-    slider.container.addEventListener('mouseenter', () => {
-      paused = true;
-    });
-    slider.container.addEventListener('mouseleave', () => {
-      paused = false;
-    });
-
-    raf = requestAnimationFrame(animate);
-  });
-
-  slider.on('destroyed', () => {
-    cancelAnimationFrame(raf);
-  });
-};
-
 export const Templates = () => {
-  const [loaded, setLoaded] = useState(false);
-  const [sliderRef] = useKeenSlider<HTMLDivElement>(
-    {
-      loop: true,
-      mode: 'free-snap',
-      slides: {
-        spacing: 24,
-        perView: 1.1,
-      },
-      breakpoints: {
-        '(min-width: 640px)': {
-          slides: {
-            perView: 1.2,
-            spacing: 24,
-          },
-        },
-        '(min-width: 768px)': {
-          slides: {
-            perView: 1.5,
-            spacing: 24,
-          },
-        },
-        '(min-width: 1024px)': {
-          slides: {
-            perView: 2.2,
-            spacing: 24,
-          },
-        },
-        '(min-width: 1280px)': {
-          slides: {
-            perView: 2.95,
-            spacing: 24,
-          },
-        },
-      },
-      created: () => setLoaded(true),
-    },
-    [ContinuousAutoscroll],
-  );
-
   return (
     <div className="mx-auto flex w-full max-w-screen-xl flex-col items-center px-24 py-48 text-center sm:py-[75px]">
       <Badge>
@@ -106,81 +22,48 @@ export const Templates = () => {
         Explore social agents and start chatting.
       </p>
 
-      <div className="relative w-full overflow-hidden">
-        {/* Left Fade */}
-        <div className="pointer-events-none absolute left-0 top-0 z-10 h-full w-[20%] bg-gradient-to-r from-neutral-1 to-transparent md:block" />
-
-        {/* Right Fade */}
-        <div className="pointer-events-none absolute right-0 top-0 z-10 h-full w-[20%] bg-gradient-to-l from-neutral-1 to-transparent md:block" />
-
-        <div
-          ref={sliderRef}
-          className={cn(
-            'keen-slider mt-56 cursor-grab active:cursor-grabbing',
-            {
-              'opacity-0': !loaded,
-              'opacity-100': loaded,
-            },
-          )}
-        >
-          {publicAgents.map((template) => (
-            <div key={template.id} className="keen-slider__slide">
-              <TemplateCard template={template} />
-            </div>
-          ))}
-        </div>
+      <div className="grid w-full max-w-[800px] gap-12 pt-40 sm:grid-cols-2 md:grid-cols-3">
+        {publicAgents.map((template) => (
+          <div key={template.id}>
+            <TemplateCard template={template} />
+          </div>
+        ))}
       </div>
     </div>
   );
 };
 
-export const TemplateCard = ({ template }: { template: PublicAgent }) => {
-  function formatCompactNumber(value: number): string {
-    return new Intl.NumberFormat('en', {
-      notation: 'compact',
-      maximumFractionDigits: 1,
-    }).format(value);
-  }
-
-  return (
-    <div className="flex overflow-clip rounded-12 border border-neutral-6 bg-neutral-2">
-      <img
-        src={template.image}
-        alt="Agent avatar"
-        className="size-[152px] shrink-0 object-cover"
-      />
-      <div className="flex w-full flex-col items-start justify-between gap-10 px-12 py-16">
-        <div className="flex w-full items-start justify-between">
-          <div className="text-left">
-            <Text variant="subtitle">{template.name}</Text>
-          </div>
-          <div className="hidden rounded-full border border-neutral-6 bg-neutral-1 px-8 py-4 text-12 text-neutral-12 sm:block">
-            {template.category}
-          </div>
+export const TemplateCard = ({ template }: { template: PublicAgent }) => (
+  <a
+    href={`preview/${template.id}`}
+    className="group relative flex h-[460px] flex-col justify-between overflow-clip rounded-16 border border-neutral-6"
+  >
+    <img
+      src={template.image}
+      alt={template.name}
+      className="absolute inset-0 -z-1 min-w-[259px] w-full h-[460px] object-cover transition-all group-hover:scale-[1.02] group-hover:opacity-80"
+      loading="lazy"
+    />
+    <div className="mr-auto flex w-full items-start justify-between p-12">
+      <div>
+        <div className="flex h-24 items-center justify-center rounded-full bg-black/60 px-8 font-medium text-neutral-12">
+          {template.category}
         </div>
-        <div className="grid w-full grid-cols-3 gap-6">
-          {/*template.socials.map((social) => (
-            <div
-              key={`${template.name} - ${social.name}`}
-              className="flex items-center justify-center gap-4 rounded-8 bg-gray-dark-4 p-8"
-            >
-              <img
-                src={social.logo}
-                alt="Social media logo"
-                className="size-18"
-              />
-            </div>
-          ))*/}
-        </div>
-        <a
-          href={`preview/${template.id}`}
-          className="flex w-full select-none items-center justify-center gap-6 rounded-8 bg-white p-8 font-medium text-black"
-        >
-          <IoChatbubbleEllipsesOutline className="shrink-0" />
-          <span className="sm:hidden"> Chat with {template.name}</span>
-          <span className="hidden sm:inline"> Chat with {template.name}</span>
-        </a>
       </div>
     </div>
-  );
-};
+    <div className="flex flex-col items-start gap-16 bg-gradient-to-b from-transparent via-black/80 to-black p-12">
+      <div className="space-y-4 text-left">
+        <p className="text-20 font-bold leading-20 text-neutral-12">
+          {template.name}
+        </p>
+        <p className="leading-18 text-neutral-12">{template.about}</p>
+      </div>
+      <button
+        type="button"
+        className="flex h-36 w-full items-center justify-center gap-8 rounded-12 bg-neutral-12 font-medium text-gray-dark-1 transition-colors group-hover:bg-neutral-11"
+      >
+        <PiChatCircleDotsBold className="size-16" /> Chat
+      </button>
+    </div>
+  </a>
+);
--- src/components/PublicAgents/config.ts
+++ src/components/PublicAgents/config.ts
@@ -4,8 +4,8 @@ export type PublicAgent = {
   category: string;
   image: string;
   author: string;
-  /*greeting: string;
   about: string;
+  /*greeting: string;
   socials: {
     name: string;
     logo: string;
@@ -31,7 +31,8 @@ export const publicAgents: PublicAgent[] = [
     name: 'Kiki',
     status: 'Running',
     agentId: '91ab9e80-f2db-4087-befb-9a410b630a8a',
-    category: 'NSFW',
+    category: '🔞 NSFW',
+    about: 'Kiki is playful, confident, and never shy',
     image:
       'https://social-agent.dev.platform.fleeksandbox.xyz/api/v1/files/91ab9e80-f2db-4087-befb-9a410b630a8a/5395e1d3-0bbf-4ae1-8b21-9c7a7fe06b80/image-1746070774164.jpg',
     avatar:
@@ -53,7 +54,8 @@ export const publicAgents: PublicAgent[] = [
     name: 'Stephanie',
     status: 'Running',
     agentId: 'dc894a33-05e7-4fca-997e-00296612eaad',
-    category: 'Sports',
+    category: '⚽️ Sports',
+    about: 'Stephanie is your go-to sports expert',
     image:
       'https://social-agent.dev.platform.fleeksandbox.xyz/api/v1/files/dc894a33-05e7-4fca-997e-00296612eaad/a538b692-6cb8-41e8-a970-cd3cfa27a628/image-1746070839876.jpg',
     avatar:
@@ -75,7 +77,8 @@ export const publicAgents: PublicAgent[] = [
     name: 'Brianna',
     status: 'Running',
     agentId: '98a76838-c016-4295-9bc9-ff2ae273673d',
-    category: 'Therapy',
+    category: '❤️‍🩹 Therapy',
+    about: 'Brianna is your calm in the chaos',
     image:
       'https://social-agent.dev.platform.fleeksandbox.xyz/api/v1/files/98a76838-c016-4295-9bc9-ff2ae273673d/e099c7a0-c731-45c5-a80f-dab0c6d44c48/image-1746070896251.jpg',
     avatar:
@@ -97,7 +100,8 @@ export const publicAgents: PublicAgent[] = [
     name: 'Carl',
     status: 'Running',
     agentId: '0004b5ed-1713-4ba6-a2a2-4bd689ecbcc0',
-    category: 'Crypto',
+    category: '🔮 Crypto',
+    about: 'Carl is your crypto confidant.',
     image:
       'https://social-agent.dev.platform.fleeksandbox.xyz/api/v1/files/0004b5ed-1713-4ba6-a2a2-4bd689ecbcc0/1195b16f-002b-4da6-a24c-57a743ab6956/image-1746070966364.jpg',
     avatar:
@@ -119,7 +123,8 @@ export const publicAgents: PublicAgent[] = [
     name: 'Jackie',
     status: 'Running',
     agentId: '9bb62709-c606-4ddd-8225-78fbb382eb11',
-    category: 'Fashion',
+    category: '👗 Fashion',
+    about: 'Jackie is your personal style guru',
     image:
       'https://social-agent.dev.platform.fleeksandbox.xyz/api/v1/files/9bb62709-c606-4ddd-8225-78fbb382eb11/a551f7d4-e808-446e-b80e-063c83885a30/image-1746071020923.jpg',
     avatar:
@@ -141,7 +146,8 @@ export const publicAgents: PublicAgent[] = [
     name: 'Victoria',
     status: 'Running',
     agentId: '6af00700-9c66-4f7d-8815-0e30fd060f67',
-    category: 'NSFW',
+    category: '🔞 NSFW',
+    about: 'Victoria is unapologetically bold',
     image:
       'https://social-agent.dev.platform.fleeksandbox.xyz/api/v1/files/6af00700-9c66-4f7d-8815-0e30fd060f67/ca064611-429e-48b9-be61-1298a811610e/image-1746071081866.jpg',
     avatar:
